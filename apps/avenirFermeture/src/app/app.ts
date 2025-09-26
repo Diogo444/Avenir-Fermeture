d269d421834110bce091602bd33fefaf8d6dc1a4
Diogo444 <da7822000@gmail.com>
2025-09-26 19:03:54 +0200

    Ajout des éléments principaux sur la page clients

--- apps/avenirFermeture/src/app/app.ts
+++ apps/avenirFermeture/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Navbar } from './components/utils/navbar/navbar';
 
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, Navbar],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
--- apps/avenirFermeture/src/app/components/view/clients/clients.ts
+++ apps/avenirFermeture/src/app/components/view/clients/clients.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+
+
+
+
 
 @Component({
   selector: 'app-clients',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatListModule],
   templateUrl: './clients.html',
   styleUrl: './clients.css',
 })
-export class Clients {}
+export class Clients {
+
+}
