d2824626fa693f4f83032143153df4911a84a4b4
Diogo444 <da7822000@gmail.com>
2025-09-26 18:31:35 +0200

    création des composant nav et page clients

--- apps/avenirFermeture/src/app/app.routes.ts
+++ apps/avenirFermeture/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadComponent: () => import('./components/view/clients/clients').then(m => m.Clients),
+  }
+];
