d401d126a10edfb3493331be6b5ed626a4cb5a15
Diogo444 <da7822000@gmail.com>
2025-09-27 16:22:39 +0200

    Ajouter un utilisateur

--- backend/src/app/Clients/client.controller.ts
+++ backend/src/app/Clients/client.controller.ts
@@ -1,14 +1,20 @@
-﻿import { Controller, Get } from '@nestjs/common';
+﻿import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ClientService } from './client.service';
-
-
+import { CreateClientDto } from './dto/create-client.dto';
 
 @Controller('clients')
 export class ClientController {
   constructor(private readonly clientService: ClientService) {}
 
   @Get()
-  findAll() {
+  // utiliser la méthode findAll du service pour récupérer tous les clients
+  async findAll() {
     return this.clientService.findAll();
   }
+
+  @Post()
+  async create(@Body() createClientDto: CreateClientDto) {
+    return this.clientService.create(createClientDto);
+  }
 }
+
--- backend/src/app/Clients/client.service.ts
+++ backend/src/app/Clients/client.service.ts
@@ -1,7 +1,9 @@
-﻿import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Client } from './client.entity';
 import { Repository } from 'typeorm';
+
+import { Client } from './client.entity';
+import { CreateClientDto } from './dto/create-client.dto';
 
 @Injectable()
 export class ClientService {
@@ -9,7 +11,25 @@ export class ClientService {
     @InjectRepository(Client)
     private readonly clientRepository: Repository<Client>,
   ) {}
+
   async findAll(): Promise<Client[]> {
     return this.clientRepository.find({ relations: ['produits'] });
   }
+
+  async create(createClientDto: CreateClientDto): Promise<Client> {
+    const { produitIds, ...clientData } = createClientDto;
+
+    const produits =
+      produitIds && produitIds.length > 0
+        ? produitIds.map((id) => ({ id }))
+        : undefined;
+
+    const client = this.clientRepository.create({
+      ...clientData,
+      ...(produits ? { produits } : {}),
+    });
+
+    return this.clientRepository.save(client);
+  }
 }
+
