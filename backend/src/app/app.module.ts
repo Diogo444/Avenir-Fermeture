b25652d0681065779b64a12a709c85f9f0398796
Diogo444 <da7822000@gmail.com>
2025-09-27 14:26:26 +0200

    Initialisation de la base de donnée

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+// import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '3306', 10),
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASS || '',
+      database: process.env.DB_NAME || 'avenir_fermeture',
+      autoLoadEntities: true,
+      synchronize: process.env.DB_SYNC ? process.env.DB_SYNC === 'true' : true,
+      logging: process.env.TYPEORM_LOGGING === 'true',
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/app/user/user.service.ts
@@ -0,0 +1,9 @@
+﻿import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserService {
+  async findAll(): Promise<User[]> {
+    return [];
+  }
+}
