cd3d24343de9838f4b9b5e4bf2f876f6341175c6
Diogo444 <da7822000@gmail.com>
2025-10-23 18:29:29 +0200

    Design tableau client

--- apps/avenirFermeture/src/app/components/view/clients/clients.ts
+++ apps/avenirFermeture/src/app/components/view/clients/clients.ts
@@ -4,6 +4,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import {MatTableModule} from '@angular/material/table';
+
 
 import { Api } from '../../../services/api/api';
 import { CommonModule } from '@angular/common';
@@ -16,13 +18,13 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-clients',
-  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatListModule, CommonModule],
+  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatListModule, CommonModule, MatTableModule],
   templateUrl: './clients.html',
   styleUrl: './clients.css',
 })
 export class Clients implements OnInit {
   client: Client[] = [];
-
+  displayedColumns: string[] = ['lastName', 'firstName', 'email', 'phone'];
 
   private readonly api = inject(Api)
   private readonly router = inject(Router)
