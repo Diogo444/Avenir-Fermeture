0d63ade769985d0f19b50019dde4cf500aa20328
Diogo444 <da7822000@gmail.com>
2025-10-29 17:40:55 +0100

    modification pastilles titre et navbar responsive

--- apps/avenirFermeture/src/app/components/utils/navbar/navbar.ts
+++ apps/avenirFermeture/src/app/components/utils/navbar/navbar.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
-import {MatIconModule } from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { NgIf, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatIconModule, RouterLink],
+  imports: [MatIconModule, RouterLink, NgIf, NgClass],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  protected isOpen = false;
+
+  protected toggleMenu(): void {
+    this.isOpen = !this.isOpen;
+  }
+
+  protected closeMenu(): void {
+    this.isOpen = false;
+  }
+
+  protected onNavigate(): void {
+    this.closeMenu();
+  }
+}
